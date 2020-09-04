f4d002266d231980347fc37fff0ff65150f00873
Kazi Mamun <kazi299499@gmail.com>
2020-09-04 19:55:54 +0600

    third upload

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
-const Product = (props) => {
+const Product = (props) => { //props come from shop
+    const {img, name, seller, price, stock} = props.product;
     console.log(props.product);
     return (
-        <div>
-            <h3>{props.product.name}</h3>
+        <div className='product'>
+            <div className="image">
+                <img src={img} alt=""/>
+            </div>
+            <div className="details">
+                <h4>{name}</h4>
+                <br/>
+                <p>
+                    <small>by: {seller}</small>
+                </p>
+                <p>price: ${price}</p>
+                <p><small>only {stock} left in stock - order soon</small></p>
+                <button className="main-button"><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
+            </div>
         </div>
     );
 };
