4f6fdd64f10492da018c02af6f83e605f3777378
Kazi Mamun <kazi299499@gmail.com>
2020-09-04 20:48:42 +0600

    final upload

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -4,21 +4,24 @@ import './Cart.css';
 const Cart = (props) => {
     const cart = props.cart;
     const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
-    const tax = (totalPrice / 10).toFixed(2);
+    const tax = totalPrice / 10;
     let shipping = 12.99;
     if(totalPrice>35 || totalPrice == 0){
         shipping = 0;
     } else if( totalPrice>15){
         shipping = 4.99;
     }
+    function fixed(amount){
+       return amount.toFixed(2);
+    }
     return (
         <div>
             <h4>Order Summery</h4>
             <p>Items Ordered : {cart.length}</p>
-            <p>Total Price: {totalPrice}</p>
-            <p><small>Shipping Cost: {shipping}</small></p>
-            <p><small>Tax + Vat: {tax}</small></p>
-            <p>Total Price: {totalPrice + shipping + tax}</p>
+            <p>Total Price: {fixed(totalPrice)}</p>
+            <p><small>Shipping Cost: {fixed(shipping)}</small></p>
+            <p><small>Tax + Vat: {fixed(tax)}</small></p>
+            <p>Total Price: {fixed(totalPrice + shipping + tax)}</p>
         </div>
     );
 };
